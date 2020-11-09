b386d283fcf9118c0aa48872adb2dc295b989b24
acdhirr <acdhirr@trilobiet.nl>
2020-11-09 18:11:16 +0100

    Calculator

--- calculator/src/main/scala/calculator/Calculator.scala
+++ calculator/src/main/scala/calculator/Calculator.scala
@@ -9,13 +9,40 @@ final case class Times(a: Expr, b: Expr) extends Expr
 final case class Divide(a: Expr, b: Expr) extends Expr
 
 object Calculator extends CalculatorInterface {
+
+  /**
+   * a: 3
+   * b: 5
+   * c: a * 2
+   * d: a + b
+   * etc.
+   * NB Only one operator per cell!
+   *
+   * @param namedExpressions
+   */
   def computeValues(
       namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
+
+      for (
+        (field, signal) <- namedExpressions
+      ) yield {
+        (field, Signal( eval( signal(), namedExpressions ) ))
+      }
+
   }
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    ???
+
+    expr match {
+      case Literal(x) => x
+      case Ref(field) => eval( getReferenceExpr(field, references), references - field) // remove field from map
+      case Plus(x,y) => eval(x, references) + eval(y, references)
+      case Minus(x,y) => eval(x, references) - eval(y, references)
+      case Times(x,y) => eval(x, references) * eval(y, references)
+      case Divide(x,y) => eval(x, references) / eval(y, references)
+      case _ => Double.NaN
+    }
+
   }
 
   /** Get the Expr for a referenced variables.
